64c7b8c1897ceeb755295cc04174adcb4f3a289c
Russell Waite <russell.waite@sorted.com>
2024-01-17 22:51:25 +0000

    latest running on my dev machine... here goes

--- FunctionName1.cs
+++ FunctionName1.cs
@@ -1,30 +1,60 @@
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 
 namespace Company.Function
 {
     public class FunctionName1
     {
-        private readonly ILogger _logger;
-
-        public FunctionName1(ILoggerFactory loggerFactory)
+        [Function("PostToMe")]
+        public async Task<DualWebAndBlobResponse> WriteWebhookBody(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
-            _logger = loggerFactory.CreateLogger<FunctionName1>();
-        }
-
-        [Function("FunctionName1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            var webhookContents = string.Empty;
+            try
+            {
+                webhookContents = await new StreamReader(req.Body).ReadToEndAsync();
+                response.WriteString("Done-ski");
+            }
+            catch(Exception ex)
+            {
+                response.WriteString($"Well shit: {ex.ToString()}");
+            }
 
-            return response;
+            return new DualWebAndBlobResponse()
+            {
+                Contents = webhookContents,
+                HttpResponse = response
+            };
         }
+
+        [Function("ReadAndClear")]
+        public DualWebAndBlobResponse ReadAndClear(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+            [BlobInput("myfunctionapp9fd8/captain.hook")] string myBlob
+            )
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            
+            response.WriteString(myBlob);
+            
+            return new DualWebAndBlobResponse()
+            {
+                Contents = string.Empty,
+                HttpResponse = response
+            };
+            
+        }
+    }
+
+    public class DualWebAndBlobResponse
+    {
+        [BlobOutput("myfunctionapp9fd8/captain.hook", Connection = "AzureWebJobsStorage")]
+        public string Contents { get; set; }
+        public HttpResponseData HttpResponse { get; set; }
     }
 }
